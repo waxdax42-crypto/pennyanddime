5b374e1a6f29fbec3a990a84fb121ab8ee136fda
waxdax42-crypto <waxdax42@gmail.com>
2026-06-11 20:15:17 +0300

    Consolidate alert messages in script2.js

--- script2.js
+++ script2.js
@@ -1,6 +1,5 @@
+alert("FOR BEST EFFECTS, Please Use a laptop😀And a WIFI Connection")
 
-
-alert("The page Won't update without you adding photos")
-alert("To download, tap, then right click, then save as...")
+alert("The page Won't update without you adding photos, To download, tap, then right click, then save as...")
 alert("Please send me more...   Love You💒")
 
